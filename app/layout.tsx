4361176afb036a69d641fc32bcd6d592561c8b5a
axel <axelker@outlook.fr>
2025-06-04 15:54:54 +0200

    feat: create base project

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import '@/app/(features)/_shared/styles/global.css';
+
+import { inter } from '@/app/(features)/_shared/fonts';
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
