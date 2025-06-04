f18ab87a76c83f294053ddd351889fdee498413a
axel <axelker@outlook.fr>
2025-06-04 17:07:13 +0200

    feat: use data base for display data inside dashboard

--- app/(features)/dashboard/page.tsx
+++ app/(features)/dashboard/page.tsx
@@ -1,3 +1,37 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { Card } from '@/app/(features)/dashboard/_components/cards';
+import RevenueChart from '@/app/(features)/dashboard/_components/revenue-chart';
+import LatestInvoices from '@/app/(features)/dashboard/_components/latest-invoices';
+import { lusitana } from '@/app/(features)/_shared/fonts';
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '@/app/(features)/_services/data';
+ 
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const {
+      numberOfCustomers,
+      numberOfInvoices,
+      totalPaidInvoices,
+      totalPendingInvoices
+  }= await fetchCardData();
+  const latestInvoices = await fetchLatestInvoices();
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        { <Card title="Collected" value={totalPaidInvoices} type="collected" /> }
+        { <Card title="Pending" value={totalPendingInvoices} type="pending" /> }
+        { <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> }
+        { <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> }
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue}  />
+        { <LatestInvoices latestInvoices={latestInvoices} /> }
+      </div>
+    </main>
+  );
 }
