2263f61268c7b86520accdb8a5f9f3373cb305e3
axel <axelker@outlook.fr>
2025-06-08 01:45:53 +0200

    fix: middleware into root

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-import { headers } from "next/headers";
-import { auth } from "@/lib/auth";
+import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
-	const session = await auth.api.getSession({
-		headers: await headers()
-	})
-
-	if (!session) {
+	const cookies = getSessionCookie(request);
+	if (!cookies) {
 		return NextResponse.redirect(new URL("/login", request.url));
 	}
-
 	return NextResponse.next();
 }
 
 export const config = {
-	runtime: "nodejs",
 	matcher: ["/((?!login|signup|api|_next|favicon.ico|assets|public).*)",], // Apply middleware to specific routes. ex. [/dashboard,..]
 };
