3250f4ebdff26e813397b25151decaaceac65aef
axel <axelker@outlook.fr>
2025-06-09 12:29:06 +0200

    feat: improve middleware for redirect logged user to dashboard

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,23 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
+
+const unauthenticatedRoutes  : string [] = ["/login", "/signup", "/"];
+
 export async function middleware(request: NextRequest) {
 	const cookies = getSessionCookie(request);
-	if (!cookies) {
-		return NextResponse.redirect(new URL("/login", request.url));
+	const url = request.nextUrl;
+	const path = url.pathname;
+	if (!cookies && !unauthenticatedRoutes.includes(path)) {
+		return NextResponse.redirect(new URL("/login", url));
+	}
+
+	if (cookies && unauthenticatedRoutes.includes(path)) {
+		return NextResponse.redirect(new URL("/dashboard", url));
 	}
 	return NextResponse.next();
 }
 
 export const config = {
-	matcher: ["/((?!login|signup|api|_next|favicon.ico|assets|public).*)",], // Apply middleware to specific routes. ex. [/dashboard,..]
+	matcher: ["/((?!api|_next|favicon.ico|assets|public).*)"], // Apply middleware to specific routes. ex. [/dashboard,..]
 };
