6fb37b64720780749562deb70250d01deea3c9d2
axel <axelker@outlook.fr>
2025-06-09 13:22:54 +0200

    feat: add auth conditional home button

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,13 @@ import Link from 'next/link';
 import styles from '@/app/(features)/_shared/styles/home.module.css';
 import { lusitana } from '@/app/(features)/_shared/fonts';
 import Image from 'next/image';
+import { auth } from "@/lib/auth"
+import { headers } from 'next/headers';
+export default async function Page() {
 
-export default function Page() {
+  const session = await auth.api.getSession({headers:await headers()})
+  const isAuthenticated = !!session;
+
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -23,10 +28,11 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
           <Link
-            href="/login"
+            href={isAuthenticated ? '/dashboard' : '/login'}
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>{isAuthenticated ? 'Dashboard' : 'Log in'}</span>
+            <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
@@ -34,6 +40,7 @@ export default function Page() {
             <div>
               <Image
               src="/hero-desktop.png"
+              priority
               width={1000}
               height={760}
               className="hidden md:block"
@@ -41,6 +48,7 @@ export default function Page() {
             />
              <Image
               src="/hero-mobile.png"
+              priority
               width={560}
               height={620}
               className="md:hidden"
