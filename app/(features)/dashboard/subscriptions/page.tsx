42361d0fee1ee0f047aae8e380528ada9ee1bbb7
axel <axelker@outlook.fr>
2025-06-11 01:45:39 +0200

    feat: add suscriptions card

---
+++ app/(features)/dashboard/subscriptions/page.tsx
@@ -0,0 +1,21 @@
+import { lusitana } from "@/app/fonts/fonts";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import Subscriptions from "@/app/(features)/dashboard/subscriptions/_components/subscriptions";
+
+export default async function Page() {
+  const activeSubs = await auth.api.listActiveSubscriptions({ headers: await headers() });
+  const activeSub = activeSubs?.find(
+    (sub) => sub.status === 'active' || sub.status === 'trialing'
+  );
+
+
+  return (
+    <>
+      <div className="w-full items-center">
+        <h1 className={`${lusitana.className} text-2xl`}>Subscription</h1>
+        <Subscriptions activeSub={activeSub} />
+      </div>
+    </>
+  )
+}
